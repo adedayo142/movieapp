8c56a3cf4f9b0dd7f17d0576c152e91cca3e78f8
Onipede Adedayo Samson <50950766+onipedadedayo142@users.noreply.github.com>
2020-06-08 10:56:17 +0100

    deploy build

---
+++ precache-manifest.675d1ec5ff12ba275411d98523c48e9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44458748bc784b1db3ebb190dea54b14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6b1209151fcfe634d0b",
+    "url": "/static/css/main.b1c3b47f.chunk.css"
+  },
+  {
+    "revision": "aebadb8625224d7be043",
+    "url": "/static/js/2.d0c3cb79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d0c3cb79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6b1209151fcfe634d0b",
+    "url": "/static/js/main.7cae3fc8.chunk.js"
+  },
+  {
+    "revision": "3d0d9c0d7b9b39e22b3d",
+    "url": "/static/js/runtime-main.724106fe.js"
+  }
+]);
